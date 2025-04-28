174c2a4be369753ef21facfd6e06296ffc116c7a
gondo <331103+gondo@users.noreply.github.com>
2025-04-28 10:59:02 +0100

    update

--- apps/git.sh
+++ apps/git.sh
@@ -4,16 +4,17 @@
 
 git config --global user.name "gondo"
 git config --global user.email "331103+gondo@users.noreply.github.com"
+git config --global user.signingkey /Users/gondo/.ssh/github-macbook-m4
 git config --global credential.helper osxkeychain
-git config --global user.signingkey /Users/gondo/.ssh/github-macbook-m4.pub
 git config --global core.excludesfile ~/.gitignore_global
 git config --global core.ignorecase true
-git config --global core.editor vi
+git config --global core.editor vim
 git config --global init.defaultbranch main
 git config --global commit.gpgsign true
 git config --global gpg.format ssh
 git config --global url.ssh://git@gitlab.com/.insteadof https://gitlab.com/
 git config --global url.ssh://git@bitbucket.org/.insteadof https://bitbucket.org/
 git config --global url.git@github.com:.insteadof https://github.com/
-git config --global url.git@bitbucket.org:.insteadof https://bitbucket.org/
+git config --global pull.rebase true
+git config --global submodule.recurse true
 git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
