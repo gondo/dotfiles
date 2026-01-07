39459dd82dc16a590b463f5916a0688bc425b4bf
gondo <331103+gondo@users.noreply.github.com>
2026-01-07 14:06:20 +0000

    added GoLand plugin

--- apps/goland.sh
+++ apps/goland.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
 
-goland installPlugins awesome.console.x
+goland installPlugins \
+    awesome.console.x \
+    cc.allape.caddyfile \
+    org.toml.lang \
+    com.jetbrains.twig \
+    net.sjrx.intellij.plugins.systemdunitfiles \
+    Docker \
+    com.intellij.react \
+    name.kropp.intellij.makefile \
+    org.jetbrains.plugins.yaml \
+    com.intellij.swagger
 
 echo "Ignore WARNings"
